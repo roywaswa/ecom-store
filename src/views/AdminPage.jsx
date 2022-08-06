0a3104d6402c43f82ad9fd412f9145012c0ff4b0
roywaswa <roywaswa.rw@gmail.com>
2022-08-06 11:05:39 +0300

    sign in form added

--- src/views/AdminPage.jsx
+++ src/views/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 
 // TODO: Add authentication with email and password
@@ -6,7 +6,17 @@ import { AuthContext } from '../contexts/AuthContext'
 
 
 function AdminPage() {
-  const {authState, setAuthState} = useContext(AuthContext)
+  const { authState, setAuthState } = useContext(AuthContext)
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+
+  function loginUser(ev) {
+    ev.preventDefault()
+    console.log(form);
+  }
   if (authState) {
     return (
       <>
@@ -19,6 +29,17 @@ function AdminPage() {
     return (
       <>
         <h1>SIGN IN</h1>
+        <form onSubmit={ev => loginUser(ev)} action="submit" method="post">
+          <div className="formfield">
+            <label htmlFor="email">Email</label>
+            <input onChange={(ev)=>{setForm({...form, email:ev.target.value})}} type="email" name="email" id="email" />
+          </div>
+          <div className="formfield">
+            <label htmlFor="password">Password</label>
+            <input onChange={(ev)=>{setForm({...form, password:ev.target.value})}} type="password" name="password" id="password" />
+          </div>
+          <button type="submit">LOG IN</button>
+        </form>
       </>
     )
   }
