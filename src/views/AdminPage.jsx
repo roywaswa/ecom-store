13d7d1f3b81c94bf993a76bab2406665e327f7f4
roywaswa <roywaswa.rw@gmail.com>
2022-08-07 14:31:08 +0300

    styled the admin page

--- src/views/AdminPage.jsx
+++ src/views/AdminPage.jsx
@@ -92,30 +92,31 @@ function AddItemModal({ setAddItemModal }) {
       <div className="container">
         <h1>ADD ITEM</h1>
         <form onSubmit={(ev) => {saveNewItem(ev)}} action="submit">
-          <div className="formfield">
+          <div className="formfield grid-col-span-2">
             <label hidden htmlFor="title">Title</label>
             <input placeholder='Title' value={form.title} onChange={(ev) => {setForm({...form, title: ev.target.value})}} type="text" name="title" id="title" />
+          </div>
+          <div className="formfield grid-col-span-2">
+            <label hidden htmlFor="price">Price</label>
+            <input placeholder='Price' onChange={(ev) => {setForm({...form, price: ev.target.value})}} value={form.price} type="number" name="price" id="price" />
+          </div>
+          <div className="formfield description grid-col-span-4">
+            <label htmlFor="description">Description</label>
+            <ReactQuill theme='snow' value={form.description} onChange={(ev) => {setForm({...form, description: ev})}} />
           </div>
           <div className="formfield">
             <label htmlFor="thumbnain">Thumbnail</label>
             <input value={form.thumbnail} onChange={(ev) => {setForm({...form, thumbnail: ev.target.value})}} type="file" name="thumbnail" id="thumbnail" />
           </div>
-          <div className="formfield">
-            <label htmlFor="description">Description</label>
-            <ReactQuill theme='snow' value={form.description} onChange={(ev) => {setForm({...form, description: ev})}} />
-          </div>
-          <div className="formfield">
-            <label hidden htmlFor="price">Price</label>
-            <input placeholder='Price' onChange={(ev) => {setForm({...form, price: ev.target.value})}} value={form.price} type="number" name="price" id="price" />
+          <div className="formfield grid-col-span-2">
+            <label hidden htmlFor="category">Category</label>
+            <input placeholder='Category' onChange={(ev) => {setForm({...form, category: ev.target.value})}} value={form.category} type="text" name="category" id="category" />
           </div>
           <div className="formfield">
             <label hidden htmlFor="inventoryQuantity">Quantity</label>
             <input placeholder='Quantity' onChange={(ev) => {setForm({...form,inventoryQuantity: ev.target.value})}} value={form.inventoryQuantity} type="number" name="inventoryQuantity" id="inventoryQuantity" />
           </div>
-          <div className="formfield">
-            <label hidden htmlFor="category">Category</label>
-            <input placeholder='Category' onChange={(ev) => {setForm({...form, category: ev.target.value})}} value={form.category} type="text" name="category" id="category" />
-          </div>
+          
           <button type="submit">SAVE ITEM</button>
         </form>
       </div>
