26bae995d07792b433f87b24360c5a5d4f188c22
roywaswa <roywaswa.rw@gmail.com>
2022-08-10 20:45:52 +0300

    product card update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,6 +14,11 @@ export default function ProductCard({ product }) {
         <span className="product-category">{product.category}</span>
         <span className="product-price">Ksh.{product.price}</span>
       </div>
+      <div className="product-controls">
+        <div className="edit"><button>edit</button></div>
+        <div className="restock"><button>restock</button></div>
+        <div className="remove"><button>delete</button></div>
+      </div>
     </div>
   );
 }
